b9abe4ba978f1e4f64ad62ad64499aa0ccf3c38d
Brandon Ly <brandonly@lostcoding.com>
2023-01-28 13:55:49 -0600

    add tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,25 @@
 describe("index test", () => {
   it("fails because I don't know what to test", () => {
-    throw new Error("no test");
+    // creating fake code to simulate what ponch should do
+
+    function call<T, P = unknown[]>(func: (...params: P[]) => T) {
+      let result;
+      try {
+        result = func(params);
+      } catch (error) {
+        result = error;
+      }
+
+      return result;
+    }
+
+    const getStuff: () => Promise<unknown> = () =>
+      Promise.resolve(
+        setTimeout(() => {
+          console.log("hello!");
+        }, 2000)
+      );
+
+    getStuff();
   });
 });
